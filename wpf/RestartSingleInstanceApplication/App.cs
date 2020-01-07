1ac014a72455d858cb4aaee370bbcb000ddb4468
James Sun <sunfei@cardioray.com.cn>
2020-01-07 14:20:37 +0800

    [add] restart-single-instance-wpf-app

---
+++ wpf/RestartSingleInstanceApplication/App.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Shell;
+
+namespace RestartSingleInstanceApplication
+{
+    public class App : Application, ISingleInstanceApp
+    {
+        private static Mutex _singleInstanceMutex;
+        private static bool _restartApp = false;
+        private static string _applicationId = "62EF6B63-36EB-4B9B-A132-762A9FDA3B2E";
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            // while (true)
+            // {
+            //     if (!SingleInstance<App>.InitializeAsFirstInstance(_applicationId))
+            //     {
+            //         Thread.Sleep(1000);
+            //         continue;
+            //     }
+            //
+            //     break;
+            // }
+            //
+            // var app = new App();
+            // app.Run();
+            //
+            // //释放 mutex && ipc channel
+            // SingleInstance<App>.Cleanup();
+
+            while (true)
+            {
+                _singleInstanceMutex = new Mutex(false, _applicationId, out var firstInstance);
+                if (firstInstance)
+                {
+                    var app = new App();
+                    app.Run();
+                }
+                else if(!_restartApp)
+                {
+                    return;
+                }
+            
+                Thread.Sleep(2000);
+            }
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public App()
+        {
+            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Current.MainWindow = new MainWindow();
+            Current.MainWindow.Show();
+        }
+    }
+}
---
+++ wpf/RestartSingleInstanceApplication/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace RestartSingleInstanceApplication
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown(0);
+        }
+    }
+}
